aa534fd6162d83b84bccd74efdd3e8436b5ad951
Katrina Hockman <khockman90@hotmail.com>
2019-01-11 09:18:37 -0800

    Add age calc functions, pass 5/5 tests

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -2,31 +2,31 @@
 import { Age } from './../src/age';
 
 describe('Age', function() {
-  //tests written to fail
+
   it('should create a new Age object', function() {
     let newAge = new Age(28, 'f', 'usa');
     expect(newAge.age).toEqual(28);
   });
-  //
-  // it('should calculate age on Mercury', function() {
-  //   let newAge = new Age(28, 'f', 'usa');
-  //   expect(newAge.conversion()).toEqual(28);
-  // });
-  //
-  // it('should calculate age on Venus', function() {
-  //   let newAge = new Age(28, 'f', 'usa');
-  //   expect(newAge.conversion()).toEqual(28);
-  // });
-  //
-  // it('should calculate age on Mars', function() {
-  //   let newAge = new Age(28, 'f', 'usa');
-  //   expect(newAge.conversion()).toEqual(28);
-  // });
-  //
-  // it('should calculate age on Jupiter ', function() {
-  //   let newAge = new Age(28, 'f', 'usa');
-  //   expect(newAge.conversion()).toEqual(28);
-  // });
+
+  it('should calculate age on Mercury', function() {
+    let newAge = new Age(28, 'f', 'usa');
+    expect(newAge.ageConversionMercury()).toEqual('116.67');
+  });
+
+  it('should calculate age on Venus', function() {
+    let newAge = new Age(28, 'f', 'usa');
+    expect(newAge.ageConversionVenus()).toEqual('45.16');
+  });
+
+  it('should calculate age on Mars', function() {
+    let newAge = new Age(28, 'f', 'usa');
+    expect(newAge.ageConversionMars()).toEqual('14.89');
+  });
+
+  it('should calculate age on Jupiter ', function() {
+    let newAge = new Age(28, 'f', 'usa');
+    expect(newAge.ageConversionJupiter()).toEqual('2.36');
+  });
   //
   // it('should calculate avg life expectancy', function() {
   //   let newAge = new Age(28, 'f', 'usa');
--- src/age.js
+++ src/age.js
@@ -6,14 +6,33 @@ class Age {
     this.country = country
   }
 
-  conversion(){
-
+  ageConversionMercury() {
+    const mercuryAge = (this.age / 0.24).toFixed(2);
+    return mercuryAge;
   }
 
-  lifeEx() {
-
-
+  ageConversionVenus() {
+    const venusAge = (this.age / 0.62).toFixed(2);
+    return venusAge;
   }
+
+  ageConversionMars() {
+    const marsAge = (this.age / 1.88).toFixed(2);
+    return marsAge;
+  }
+
+  ageConversionJupiter() {
+    const jupiterAge = (this.age / 11.86).toFixed(2);
+    return jupiterAge;
+  }
+
+
+
+
+  // lifeEx() {
+  //
+  //
+  // }
 
 
 }
