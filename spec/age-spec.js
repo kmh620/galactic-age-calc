78ebb307d3118ef0b3441c20f00adc8e3de16610
Katrina Hockman <khockman90@hotmail.com>
2019-01-11 08:51:37 -0800

    Pass first test

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,35 +1,37 @@
 
+import { Age } from './../src/age';
+
 describe('Age', function() {
   //tests written to fail
   it('should create a new Age object', function() {
     let newAge = new Age(28, 'f', 'usa');
-    expect(newAge.age).toEqual(29);
+    expect(newAge.age).toEqual(28);
   });
-
-  it('should calculate age on Mercury', function() {
-    let newAge = new Age(28, 'f', 'usa');
-    expect(newAge.conversion()).toEqual(28);
-  });
-
-  it('should calculate age on Venus', function() {
-    let newAge = new Age(28, 'f', 'usa');
-    expect(newAge.conversion()).toEqual(28);
-  });
-
-  it('should calculate age on Mars', function() {
-    let newAge = new Age(28, 'f', 'usa');
-    expect(newAge.conversion()).toEqual(28);
-  });
-
-  it('should calculate age on Jupiter ', function() {
-    let newAge = new Age(28, 'f', 'usa');
-    expect(newAge.conversion()).toEqual(28);
-  });
-
-  it('should calculate avg life expectancy', function() {
-    let newAge = new Age(28, 'f', 'usa');
-    //81.6 === true
-    expect(newAge.lifeEx()).toEqual(80);
-  });
+  //
+  // it('should calculate age on Mercury', function() {
+  //   let newAge = new Age(28, 'f', 'usa');
+  //   expect(newAge.conversion()).toEqual(28);
+  // });
+  //
+  // it('should calculate age on Venus', function() {
+  //   let newAge = new Age(28, 'f', 'usa');
+  //   expect(newAge.conversion()).toEqual(28);
+  // });
+  //
+  // it('should calculate age on Mars', function() {
+  //   let newAge = new Age(28, 'f', 'usa');
+  //   expect(newAge.conversion()).toEqual(28);
+  // });
+  //
+  // it('should calculate age on Jupiter ', function() {
+  //   let newAge = new Age(28, 'f', 'usa');
+  //   expect(newAge.conversion()).toEqual(28);
+  // });
+  //
+  // it('should calculate avg life expectancy', function() {
+  //   let newAge = new Age(28, 'f', 'usa');
+  //   //81.6 === true
+  //   expect(newAge.lifeEx()).toEqual(80);
+  // });
 
 });
---
+++ src/age.js
@@ -0,0 +1,21 @@
+
+class Age {
+  constructor(age, gender, country) {
+    this.age = age,
+    this.gender = gender,
+    this.country = country
+  }
+
+  conversion(){
+
+  }
+
+  lifeEx() {
+
+
+  }
+
+
+}
+
+export { Age };
